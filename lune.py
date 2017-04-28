b4a8df71d1502f5ef0ae935ff275438c089176cc
Félix Defrance <felixgif@gmail.com>
2017-04-28 09:55:32 +0200

    Update lune.py

--- lune.py
+++ lune.py
@@ -3,84 +3,72 @@ import math
 
 turtle.setworldcoordinates(-500000000,-500000000,500000000,500000000)
 
+#soleil
+ts = turtle.Turtle()
+ts.shape('circle')
+ts.color('yellow')
+ts.speed(400)
+ts.shapesize(2)
 
-# création de la Lune
-
-t1=turtle.Turtle()  
+#lune
+t1 = turtle.Turtle()  
 t1.shape('circle')
 t1.color('grey')
 t1.speed(400)
-t1.shapesize(1)
+t1.shapesize(3)
 
-# création de la Terre
-
+#terre
 t2=turtle.Turtle()  
 t2.shape('circle')
 t2.color('blue')
 t2.speed(400)
-t2.shapesize(3)
+t2.shapesize(1)
 
-# création de la fusée
-
-t3=turtle.Turtle()
-t3.color('red')
-t3.speed(400)
-
-# données de la fusée
-   
-F=9000000 # norme du vecteur vitesse constant
-xx=0
-yy=0
-Vx=0
-Vy=F
-
-# donnés astronomiques
-
+#donnees
 m= 7*10**22
 G= 6.67*10**-11
 M=6*10**24
 
-
-# initialisation des données de la lune
-
+#terre init
 x=0        
-y=450000000
+y=400000000
 vx=1000
 vy=0
 ax=0
 ay=0
 dt = 10000
 
-t1.pu()
-t1.goto(x,y)
-t1.pd()
+#lune init
+X= 0 
+Y= int(y + 1000)
+vX= vx + 500
+vY=0
+aX=0
+aY=0
+dT = 10000
 
-
-    
-
+#soleil data
+#ts.pu()
+#ts.goto(xx,yy)
+#ts.pd()
 
 for i in range (1000000):
 
-    ax = -( G*M*x)/(x**2+y**2)**1.5
-    ay = -( G*M*y)/(x**2+y**2)**1.5
-    vx=ax*dt+vx
-    vy=ay*dt+vy
-    x=vx*dt+x
-    y=vy*dt+y
-    t1.goto(x,y)
+   ts.goto(0,0)
+   ax = -( G*M*x)/(x**2+y**2)**1.5
+	ay = -( G*M*y)/(x**2+y**2)**1.5
+	vx+=ax*dt
+	vy+=ay*dt
+   x+=vx*dt
+   y+=vy*dt
+	t2.goto(x,y)
 
-    r=math.sqrt((xx-x)**2+(yy-y)**2)
-    Vx=-F*(xx-x)/r
-    Vy=-F*(yy-y)/r
-    xx=Vx+xx
-    yy=Vy+yy
-    t3.goto(xx,yy)
-
-
-
-
-
-
-
+	aX = -(G*M*X)/(X**2+Y**2)**1.5
+   ay = -(G*M*Y)/(X**2+Y**2)**1.5
+   vX+=aX*dt
+   vY+=aY*dt
+   X+=vX*dt
+   Y+=vY*dt
+   t1.goto(X,Y)
 
 turtle.exitonclick()
